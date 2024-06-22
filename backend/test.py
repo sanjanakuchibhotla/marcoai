9e8993725cac8b00208ed08dfb6982e28f469141
sanjanakuchibhotla <skuchib2@andrew.cmu.edu>
2024-06-22 15:03:57 -0700

    test you.com

---
+++ backend/test.py
@@ -0,0 +1,16 @@
+import os
+
+from langchain.retrievers.you import YouRetriever
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["YDC_API_KEY"] = os.getenv('YOU_API_KEY')
+os.environ["OPENAI_API_KEY"] = os.getenv('OPEN_AI_KEY')
+yr = YouRetriever()
+model = "gpt-3.5-turbo-16k"
+qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model=model), chain_type="stuff", retriever=yr)
+
+print(qa.run("how was the New York City pinball ban lifted?"))
