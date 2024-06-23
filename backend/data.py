12f910d34b41af9b2df2d2357b6942429efaa5da
Sanjna Enjeeti <senjeeti@Sanjnas-MacBook-Pro.local>
2024-06-23 01:21:56 -0700

    parsed user input

--- backend/data.py
+++ backend/data.py
@@ -1,8 +1,8 @@
 adventure_ratings = {
-    "beaches": {"overall_rating": 10, "average_rating": 6, "total_number": 5},
-    "nightlife": {"overall_rating": 10, "average_rating": 6, "total_number": 4},
-    "landmarks": {"overall_rating": 8, "average_rating": 7, "total_number": 7},
-    "museums": {"overall_rating": 5, "average_rating": 3, "total_number": 3},
-    "food": {"overall_rating": 9, "average_rating": 8, "total_number": 10},
-    "architecture": {"overall_rating": 7, "average_rating": 5, "total_number": 6}
+    "beaches": {"overall_rating": 30, "average_rating": 6, "total_number": 5},
+    "nightlife": {"overall_rating": 24, "average_rating": 6, "total_number": 4},
+    "landmarks": {"overall_rating": 49, "average_rating": 7, "total_number": 7},
+    "museums": {"overall_rating": 9, "average_rating": 3, "total_number": 3},
+    "food": {"overall_rating": 80, "average_rating": 8, "total_number": 10},
+    "architecture": {"overall_rating": 30, "average_rating": 5, "total_number": 6}
 }
--- backend/search.py
+++ backend/search.py
@@ -1,33 +1,40 @@
-# import langchain
-# import os
-# # The YDC_API_KEY and OPENAI_API_KEY should be defined in a .env file
-# # Let's load the API keys in from the .env file
-# import dotenv
-# from langchain_community.retrievers.you import YouRetriever
-
-# ydc_retriever = YouRetriever(num_web_results = 10)
-
-
-# dotenv.load_dotenv(".env", override=True)
-
-# # Let's test it out
-# response = ydc_retriever.invoke("Has the inflation in Canada dropped in 2024?")
-# # Let's take a look at the first 3 responses
-# print(response[:3])   
-
 import os
-
+import json
+from langchain_core.documents import Document
+from typing import TYPE_CHECKING, Any, Dict, List, Optional
 from langchain.retrievers.you import YouRetriever
 from langchain.chains import RetrievalQA
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import data
+import dspy
 
 load_dotenv()
 
 os.environ["YDC_API_KEY"] = os.getenv('YOU_API_KEY')
 os.environ["OPENAI_API_KEY"] = os.getenv('OPEN_AI_KEY')
+
+
+userPreference = input("Do you have ideas for an adventure?\n")
+location = input("Where are you located?\n")
+
+prefs = {k: v for k, v in sorted(data.adventure_ratings.items(), key=lambda item: item[1]["average_rating"], reverse=True)}
+prefsList = prefs.keys()
+userLikes = "The user enjoys, from most to least, " + ', '.join(prefsList) + '.'
+searchPrompt = userLikes + " The user inputted the following for their ideas: " + userPreference + "\n Can you give suggestions near " + location + " ? Return a json string where each suggestion has the following format that I can turn into python: {Place: string; City: string; Description: string; Distance: number; Price: (range from $ to $$$$); Resources/Links: string} "
+
 yr = YouRetriever()
+model = "gpt-4o"
+qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model=model), chain_type="stuff", retriever=yr)
+data = qa.run(searchPrompt)
+data = data.strip('json\n')
+print(data)
 
+# json.loads(data)
 
-response = yr.invoke("If you are in San Francisco for a trip, where could you go? Suggest affordable ideas and include prices.")
-print(response[:3])  
+# s
+def generateSearch(dictionary):
+    for v in dictionary:
+        print(v)
+
+# generateSearch(data)
