2034032b44150074b61f625a6702620cfdfbd4ab
harshdeep kaur <harshdeepkaur@wifi-10-40-225-104.wifi.berkeley.edu>
2024-06-22 18:31:49 -0700

    search stuff

--- backend/search.py
+++ backend/search.py
@@ -1,15 +1,10 @@
 import requests
-import os
-from dotenv import load_dotenv
+def get_ai_snippets_for_query(query): 
+    headers = {"X-API-Key": "806a3761-5d05-4fbe-a303-806f5a394c41<__>1PTsFeETU8N2v5f4qmtDZVGS"} 
+    params = {"query": query} 
+    return requests.get(f"https://api.ydc-index.io/search?query={query}",params=params, headers=headers,).json()
+query = "places to visit near san francisco"
+print(get_ai_snippets_for_query("places to visit near san francisco"))
 
-load_dotenv()
-
-def get_ai_snippets_for_query(query):
-    ydc_api_key = os.getenv('YOU_API_KEY')
-    headers = {"X-API-Key": ydc_api_key}
-    params = {"query": query}
-    return requests.get(f"https://api.ydc-index.io/search?query={query}",params=params,headers=headers,).json()
-query = "places to visit in a 10 mile radius of union square sf"
-
-print(get_ai_snippets_for_query(query))
-
+query = "places to visit near san francisco"
+requests.get(f"https://api.ydc-index.io/search?query={query}",params=params, headers=headers,).json()
