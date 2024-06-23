c9fca24793bad4e133d2e1480eb4acb2408a13b9
sanjanakuchibhotla <skuchib2@andrew.cmu.edu>
2024-06-23 01:27:12 -0700

    update ranking fn

--- backend/updateRanking.py
+++ backend/updateRanking.py
@@ -1,2 +1,17 @@
-from data import adventure_ratings
-from ratings import ratings_keywords
+import json
+
+
+def updateRating(category, newRating):
+    with open("./data.json", "r") as f:
+        data = json.load('f')
+        data[category]['overall_rating'] += newRating
+        currAverage = data[category]['average_rating']
+        totalNum = data[category]['total_number']
+        data[category]['average_rating'] = (currAverage*totalNum + newRating)/(totalNum+1)
+        data[category]['total_number'] += 1
+    with open("./data.json", 'w') as file:
+        json.dump(data, file)
+    return
+
+def __main__():
+    updateRating("beaches",8)
