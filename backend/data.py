19ddce7f40278e6100dee0bd0c93b78a00b47048
sanjanakuchibhotla <skuchib2@andrew.cmu.edu>
2024-06-22 21:50:25 -0700

    added data

---
+++ backend/data.py
@@ -0,0 +1,8 @@
+adventure_ratings = {
+    "beaches": {"overall_rating": 10, "average_rating": 6, "total_number": 5},
+    "nightlife": {"overall_rating": 10, "average_rating": 6, "total_number": 4},
+    "landmarks": {"overall_rating": 8, "average_rating": 7, "total_number": 7},
+    "museums": {"overall_rating": 5, "average_rating": 3, "total_number": 3},
+    "food": {"overall_rating": 9, "average_rating": 8, "total_number": 10},
+    "architecture": {"overall_rating": 7, "average_rating": 5, "total_number": 6}
+}
---
+++ backend/updateRanking.py
@@ -0,0 +1,2 @@
+from data import adventure_ratings
+from ratings import ratings_keywords
