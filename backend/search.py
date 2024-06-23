f079013c70d939739b62041a6db1a931f747b951
Sanjna Enjeeti <senjeeti@wifi-10-40-43-154.wifi.berkeley.edu>
2024-06-22 17:46:53 -0700

    llm examples

--- backend/search.py
+++ backend/search.py
@@ -1,17 +1,58 @@
-import requests
+# import langchain
+# import os
+# # The YDC_API_KEY and OPENAI_API_KEY should be defined in a .env file
+# # Let's load the API keys in from the .env file
+# import dotenv
+# from langchain_community.retrievers.you import YouRetriever
+
+# ydc_retriever = YouRetriever(num_web_results = 10)
+
+
+# dotenv.load_dotenv(".env", override=True)
+
+# # Let's test it out
+# response = ydc_retriever.invoke("Has the inflation in Canada dropped in 2024?")
+# # Let's take a look at the first 3 responses
+# print(response[:3])   
+
 import os
+
+from langchain.retrievers.you import YouRetriever
+from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import dspy
 
 load_dotenv()
 
+os.environ["YDC_API_KEY"] = os.getenv('YOU_API_KEY')
+os.environ["OPENAI_API_KEY"] = os.getenv('OPEN_AI_KEY')
+yr = YouRetriever()
+model = "gpt-3.5-turbo-16k"
+qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model=model), chain_type="stuff", retriever=yr)
 
-def get_ai_snippets_for_query(query):
-    ydc_api_key = os.getenv('YOU_API_KEY')
-    headers = {"X-API-Key": ydc_api_key}
-    params = {"query": query}
-    return requests.get(f"https://api.ydc-index.io/search?query={query}",params=params,headers=headers,).json()
-query = "places to visit in a 10 mile radius of union square sf based on my love for food"
+# print(qa.run("how was the New York City pinball ban lifted?"))
+response = yr.invoke("If you are in San Francisco for a trip, where could you go? Suggest affordable ideas and include prices.")
+print(response[:3])  
 
+turbo = dspy.OpenAI(model='gpt-4o')
+# thread-safe built-in
+dspy.settings.configure(lm=turbo)
 
-result = get_ai_snippets_for_query("places to visit near san francisco")
-print(result['hits'][0]['description'])
+class BasicQA(dspy.Signature):
+    """Answer questions with wise suggestions"""
+
+    question = dspy.InputField()
+    answer = dspy.OutputField(desc="often between 40-50 words")
+
+question = "If you are in San Francisco for a trip, where could you go? Suggest affordable ideas and include prices."
+
+# Define the predictor.
+generate_answer = dspy.Predict(BasicQA)
+
+# Call the predictor on a particular input.
+pred = generate_answer(question=question)
+
+# Print the input and the prediction.
+print(f"Question: {question}")
+print(f"Predicted Answer: {pred.answer}")
