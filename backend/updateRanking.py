ad3c4db94bf8774d98d54a98b756b086e77f166d
Sanjna Enjeeti <senjeeti@Sanjnas-MacBook-Pro.local>
2024-06-23 01:50:36 -0700

    fixed updateranking

--- backend/updateRanking.py
+++ backend/updateRanking.py
@@ -3,15 +3,14 @@ import json
 
 def updateRating(category, newRating):
     with open("./data.json", "r") as f:
-        data = json.load('f')
+        data = json.load(f)
         data[category]['overall_rating'] += newRating
         currAverage = data[category]['average_rating']
         totalNum = data[category]['total_number']
-        data[category]['average_rating'] = (currAverage*totalNum + newRating)/(totalNum+1)
+        data[category]['average_rating'] = int((currAverage*totalNum + newRating)/(totalNum+1))
         data[category]['total_number'] += 1
     with open("./data.json", 'w') as file:
         json.dump(data, file)
     return
 
-def __main__():
-    updateRating("beaches",8)
+updateRating("beaches",8)
