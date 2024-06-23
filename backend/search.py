4265d70cfdd88755fad100320c5b8e4756781bff
sanjanakuchibhotla <skuchib2@andrew.cmu.edu>
2024-06-22 18:29:06 -0700

    add search

---
+++ backend/search.py
@@ -0,0 +1,15 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_ai_snippets_for_query(query):
+    ydc_api_key = os.getenv('YOU_API_KEY')
+    headers = {"X-API-Key": ydc_api_key}
+    params = {"query": query}
+    return requests.get(f"https://api.ydc-index.io/search?query={query}",params=params,headers=headers,).json()
+query = "places to visit in a 10 mile radius of union square sf"
+
+print(get_ai_snippets_for_query(query))
+
