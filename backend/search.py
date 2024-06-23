66096d61ddc1671d949297c104f083d1b2fb2aef
sanjanakuchibhotla <skuchib2@andrew.cmu.edu>
2024-06-22 19:18:10 -0700

    fixes

--- backend/search.py
+++ backend/search.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_ai_snippets_for_query(query): 
-    ydc_key = os.getenv('YOU_API_KEY')
-    headers = {"X-API-Key": ydc_key} 
-    params = {"query": query} 
-    return requests.get(f"https://api.ydc-index.io/search?query={query}",params=params, headers=headers,).json()
-query = "places to visit near san francisco"
+
+def get_ai_snippets_for_query(query):
+    ydc_api_key = os.getenv('YOU_API_KEY')
+    headers = {"X-API-Key": ydc_api_key}
+    params = {"query": query}
+    return requests.get(f"https://api.ydc-index.io/search?query={query}",params=params,headers=headers,).json()
+query = "places to visit in a 10 mile radius of union square sf based on my love for food"
 
 
 result = get_ai_snippets_for_query("places to visit near san francisco")
